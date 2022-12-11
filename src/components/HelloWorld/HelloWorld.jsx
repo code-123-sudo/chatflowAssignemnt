b83389edf411b36decb617c8eff511564b26c9e2
code-123-sudo <56316151+code-123-sudo@users.noreply.github.com>
2022-12-11 23:48:26 +0530

    Add files via upload

---
+++ src/components/HelloWorld/HelloWorld.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './style.scss';
+import DiagramContainer from "./DiagramContainer";
+
+
+const HelloWorld = () => {
+	return (
+		<div>
+		 <DiagramContainer></DiagramContainer>
+		</div>
+	)
+}
+
+export default HelloWorld
